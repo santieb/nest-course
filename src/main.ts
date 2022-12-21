488512bc7b276e855a5c638f8d09e4e8c9e8388a
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-21 00:35:08 -0300

    add: config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config/dist';
+import * as morgan from 'morgan';
+import { CORS } from './constansts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.use(morgan('dev'));
+
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+  console.log(`app listening on: ${await app.getUrl()}`);
 }
 bootstrap();
